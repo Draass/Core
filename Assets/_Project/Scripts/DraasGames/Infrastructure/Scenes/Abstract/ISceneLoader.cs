923cc1b7d09dac2512a81e106664ad14af6b7f79
draas <draas.games@gmail.com>
2024-02-10 21:44:11 +0300

    Add basic scene loader

---
+++ Assets/_Project/Scripts/DraasGames/Infrastructure/Scenes/Abstract/ISceneLoader.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using UnityEngine.SceneManagement;
+
+namespace DraasGames.Infrastructure.Scenes.Abstract
+{
+    public interface ISceneLoader
+    {
+        public event Action<string> SceneLoadingBegin;
+        public event Action<string> SceneLoadingFinished;
+
+        public event Action<string> SceneUnloadingBegin;
+        public event Action<string> SceneUnloadingFinished;
+        
+        public List<Scene> ActiveScenes { get; }
+        
+        public UniTask LoadSceneAsync(string scene, bool additive = false);
+        public UniTask UnloadSceneAsync(string scene);
+    }
+}
---
+++ Assets/_Project/Scripts/DraasGames/Infrastructure/Scenes/Concrete/SceneLoader.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using Cysharp.Threading.Tasks;
+using DraasGames.Infrastructure.Scenes.Abstract;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+namespace DraasGames.Infrastructure.Scenes.Concrete
+{
+    public class SceneLoader : ISceneLoader
+    {
+        public event Action<string> SceneLoadingBegin;
+        public event Action<string> SceneLoadingFinished;
+        
+        public event Action<string> SceneUnloadingBegin;
+        public event Action<string> SceneUnloadingFinished;
+
+        public List<Scene> ActiveScenes { get; private set; }
+        
+        public async UniTask LoadSceneAsync(string scene, bool additive = false)
+        {
+            SceneLoadingBegin?.Invoke(scene);
+            
+            var previousScene = SceneManager.GetActiveScene();
+
+            if (additive)
+                await SceneManager.LoadSceneAsync(scene, LoadSceneMode.Additive);
+            else 
+                await SceneManager.LoadSceneAsync(scene, LoadSceneMode.Single);
+            
+            ActiveScenes.Add(SceneManager.GetSceneByName(scene));
+
+            if (!additive)
+            {
+                ActiveScenes.Remove(previousScene);
+            }
+
+            SceneLoadingFinished?.Invoke(scene);
+        }
+
+        public async UniTask UnloadSceneAsync(string scene)
+        {
+            SceneLoadingBegin?.Invoke(scene);
+
+            await SceneManager.UnloadSceneAsync(scene);
+            
+            SceneLoadingFinished?.Invoke(scene);
+        }
+    }
+}
